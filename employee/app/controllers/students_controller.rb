b18a3f754e5c8dccd1be6c8ef78e9eff18046183
chetna2711 <chetnakanak2001@gmail.com>
2024-08-18 11:24:46 +0530

    resolve bug

--- employee/app/controllers/students_controller.rb
+++ employee/app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: %i[show edit update destroy]
+  before_action :set_student, only: [:show, :edit, :update, :destroy]
 
   def index
     @students = Student.all
@@ -33,19 +33,14 @@ class StudentsController < ApplicationController
   end
 
   def destroy
-    if @student.destroy
-      redirect_to students_path, notice: 'Student was successfully deleted.'
-    else
-      redirect_to students_path, alert: 'Failed to delete student.'
-    end
+    @student.destroy
+    redirect_to students_path, notice: 'Student was successfully deleted.'
   end
 
   private
 
   def set_student
     @student = Student.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    redirect_to students_path, alert: 'Student not found.'
   end
 
   def student_params
